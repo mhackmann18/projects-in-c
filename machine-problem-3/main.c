4b4e0a3c4491abc6c9a79f61b6c9a43d80373ca9
Matt Hackmann <mhackmann13@icloud.com>
2020-11-16 19:15:45 -0600

    Finished machine-problem-3

--- machine-problem-3/main.c
+++ machine-problem-3/main.c
@@ -3,8 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 
-void buildMaxHeap(int arr[], int n);
-void heapify(int arr[], int n, int i);
+void swap(int arr[], int index1, int index2);
+void printHeap(int arr[]);
+void insert(int heap[], int val);
+int *buildMaxHeap(int arr[], int size);
+void maxHeapify(int arr[], int index);
+void heapChangeKey(int heap[], int index, int val);
+void delete(int heap[], int index);
 
 int main(int argc, char **argv){
   int size, n, j, k = 0;
@@ -13,81 +18,134 @@ int main(int argc, char **argv){
   // Read heap size from file
   FILE *fp = fopen(argv[1], "r");
   fscanf(fp, "%d", &size);
-
-  // Array of heap values
+  // Get all of the node values from the input and store them in an array
   int arr[size];
-
-  // Find numbers of vertices. Runs in O(E) time.
   for(int i = 0; i < size; ++i){
     fscanf(fp, "%d", &n);
     arr[i] = n;
   }
+
+  // Build a max heap from the array of node values
+  int *heap = buildMaxHeap(arr, size);
 
   while(1){
     c = fgetc(fp);
     if(c==EOF){
       break;
     } else if(c == 'E'){
-      printf("Extract Max\n");
+      delete(heap, 1);
     } else if(c == 'I'){
       c = fgetc(fp);
       fscanf(fp, "%d", &j);
-      printf("Insert key %d\n", j);
+      insert(heap, j);
     } else if(c == 'D'){
       c = fgetc(fp);
       fscanf(fp, "%d", &j);
-      printf("Delete key %d\n", j);
+      delete(heap, j);
     } else if(c == 'C') {
       c = fgetc(fp);
       fscanf(fp, "%d", &j);
       c = fgetc(fp);
       fscanf(fp, "%d", &k);
-      printf("Change key at A[%d] to %d\n", j, k);
+      heapChangeKey(heap, j, k);
     } 
   }
   fclose(fp);
+  
+  printHeap(heap);
+}
 
-  buildMaxHeap(arr, size);
+void delete(int heap[], int index){
+  int lastNode = heap[heap[0]];
+  heap[0] -= 1;
+  heapChangeKey(heap, index, lastNode);
+}
+
+void heapChangeKey(int heap[], int index, int val){
+  heap[index] = val;
+  maxHeapify(heap, index);
+}
+
+void maxHeapify(int heap[], int index){
+  int rightIndex = 2 * index + 1;
+  int leftIndex = 2 * index;
+  // if both child nodes exist
+  if(rightIndex <= heap[0] && leftIndex <= heap[0]){
+    if(heap[rightIndex] > heap[index] || heap[leftIndex] > heap[index]){
+      if(heap[rightIndex] >= heap[leftIndex]){
+        swap(heap, index, rightIndex);
+        maxHeapify(heap, rightIndex);
+      } else {
+        swap(heap, index, leftIndex);
+        maxHeapify(heap, leftIndex);
+      }
+    }
+  // if only the left child node exists
+  } else if(leftIndex <= heap[0]){
+    if(heap[leftIndex] > heap[index]){
+      swap(heap, leftIndex, index);
+      maxHeapify(heap, leftIndex);
+    }
+  }
+  if(index != 1){
+    maxHeapify(heap, index - 1);
+  }
+}
+
+// Takes an array of integers and returns a max heap with each integer as a key
+int *buildMaxHeap(int arr[], int size){
+  static int heap[1001];
+  heap[0] = 0;
+
   for(int i = 0; i < size; ++i){
+    heap[i+1] = arr[i];
+    heap[0] += 1;
+  }
+
+  maxHeapify(heap, size);
+
+  return heap;
+}
+
+// Inserts a new node with the passed in value into the heap
+void insert(int heap[], int val){
+  // Update heap size
+  heap[0] += 1;
+
+  int i = heap[0];
+
+  // Add new node with value to end of heap
+  heap[i] = val;
+
+  // If the element being inserted isn't the only element in the heap
+  if(i != 1){
+    // Get the parent node of the new node
+    int parent = i / 2;
+
+    // Keep moving the new node up the heap until it's in its correct position or it's at the top of the heap
+    while(heap[i] > heap[parent] && i != 1){
+      swap(heap, i, parent);
+      i = parent;
+      parent = i / 2;
+    }
+  } 
+}
+
+// Prints out the values of an array
+void printHeap(int arr[]){
+  for(int i = 1; i <= arr[0]; ++i){
     printf("%d ", arr[i]);
   }
   printf("\n");
 }
 
+// Takes in an array and the indeces of two nodes to be swapped, and swaps their positions.
+void swap(int arr[], int index1, int index2){
+  int newNode1Val = arr[index2];
+  int newNode2Val = arr[index1];
 
-void buildMaxHeap(int arr[], int n){ 
-  // Index of last non-leaf node 
-  int startIndex = n / 2 - 1; 
+  arr[index1] = newNode1Val;
+  arr[index2] = newNode2Val;
 
-  // Perform reverse level order traversal 
-  // from last non-leaf node and heapify 
-  // each node 
-  for (int i = startIndex; i >= 0; i--) { 
-    heapify(arr, n, i); 
-  } 
-} 
-
-void heapify(int arr[], int n, int i){ 
-  int largest = i; // Initialize largest as root 
-  int l = 2 * i + 1; // left = 2*i + 1 
-  int r = 2 * i + 2; // right = 2*i + 2 
-
-  // If left child is larger than root 
-  if (l < n && arr[l] > arr[largest]) 
-    largest = l; 
-
-  // If right child is larger than largest so far 
-  if (r < n && arr[r] > arr[largest]) 
-    largest = r; 
-
-  // If largest is not root 
-  if (largest != i) { 
-    int first = arr[i];
-    int second = arr[largest];
-    arr[i] = second;
-    arr[largest] = first;
-
-    // Recursively heapify the affected sub-tree 
-    heapify(arr, n, largest); 
-  } 
-} 
+  //printf("Swapped %d and %d\n", arr[index2], arr[index1]);
+}
